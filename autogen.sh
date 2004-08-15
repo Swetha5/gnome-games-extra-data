266d17ec2dda8c1bdf795470bc3242e80ac6bc3f
Callum McKenzie <callum@src.gnome.org>
2004-08-15 03:30:18 +0000

    Initial revision

---
+++ autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="Gnome Games Extra Data"
+
+(test -f $srcdir/configure.in \
+  && test -d $srcdir/same-gnome) || {
+    echo -n "**Error**: Directory \"\`$srcdir\'\" does not look like the"
+    echo " top-level gnome directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
+REQUIRED_AUTOMAKE_VERSION=1.7
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
